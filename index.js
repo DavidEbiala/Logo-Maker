3c5f91458ae18b142af7efeb437f4ceac79d60b0
Zino Ebiala <eebia002@fiu.edu>
2023-09-04 13:17:21 -0400

    Pushing triangle to examples

--- index.js
+++ index.js
@@ -31,10 +31,39 @@ function promptUser(){
       promptUser();
     }else{
       console.log(answer);
-      fs.writeFile('logo.svg',answer);
+      writeFile('logo.svg',answer);
     }
   });
 
+}
+
+function writeFile(fileName, answer){
+  let svg ="";
+  svg = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+  svg += '<g>';
+  svg += `${answer.shape}`;
+
+  let shapeChoice;
+  switch(answer.shape){
+    case "Triangle": 
+      shapeChoice = new Triangle();
+      svg += `<polygon points="150, 18 244, 182 56, 182" fill="${answer.color}"/>`;
+      break;
+    case "Square":
+      shapeChoice = new Square();
+      svg += `<rect x="73" y="40" width="160" height="160" fill="${answer.color}"/>`;
+      break;
+    default:
+      console.log("Choose only Square or Triangle");
+      promptUser();
+  }
+  svg += `<text x="150" y="130" text-anchor="middle" font-size="40">${answer.text}</text>`;
+svg += "</g>";
+svg += "</svg>";
+
+fs.writeFile(fileName, svg, (err) => {
+  err ? console.log(err) : console.log("Generated logo.svg");
+});
 }
 
 promptUser();
